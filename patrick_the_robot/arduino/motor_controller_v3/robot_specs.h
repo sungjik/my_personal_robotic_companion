376206252b048e5a26f5bc4a6c1eff7f73898ece
Sung Jik Cha <sungjik.cha@berkeley.edu>
2015-09-12 21:08:46 +0900

    did mapping

--- patrick_the_robot/arduino/motor_controller_v3/robot_specs.h
+++ patrick_the_robot/arduino/motor_controller_v3/robot_specs.h
@@ -2,7 +2,7 @@
 #define ROBOT_SPECS_H
 
 #define encoder_pulse   13
-#define gear_ratio      20
+#define gear_ratio      120
 #define wheel_diameter  0.065   //m
 #define wheel_width     0.027   //m
 #define track_width     0.266  //m
